d9528ee85cc1694df8168b03124c19653b7ba8a0
jaehoon song <story515@naver.com>
2022-11-21 20:55:19 +0900

    second commit

--- practice.py
+++ practice.py
@@ -7,4 +7,154 @@ print("풍선")
 print('나비')
 print("나비"*20)
 print("응가"*20)
-print(5>10)
+print(5<10)
+print(True)
+print(False)
+print(not 5>10)
+
+# 애완동물을 소개해 주세요
+animal = "강아지"
+name = "호식이"
+age = 4
+hobby = "잠"
+is_adult = age >= 3 
+
+
+
+
+
+
+
+print(" 우리집"  + animal + "있으며 이름은 "+ name + "입니다")
+print (  name + "는" + str(age) + "살이며 " + hobby + " 을 자는것을 좋아합니다.")
+print( name +"는 어른일까요 ? " + str(is_adult))
+
+# 애완동물을 소개해 주세요
+animal = "고양이"
+name = "해피"
+age = 2
+hobby = "공놀이"
+is_adult = age >= 3 
+
+
+
+
+
+
+
+print(" 우리집"  + animal + "있으며 이름은 "+ name + "입니다")
+print (  name + "는" + str(age) + "살이며 " + hobby + " 을 자는것을 좋아합니다.")
+print( name +"는 어른일까요 ? " + str(is_adult))
+
+# 애완동물을 소개해 주세요
+animal = "강아지"
+name = "호식이"
+age = 4
+hobby = "잠"
+is_adult = age >= 3 
+
+
+
+
+
+
+
+print(" 우리집"  , animal , "있으며 이름은 ", name , "입니다")
+print (  name , "는" ,age , "살이며 " + hobby , " 을 자는것을 좋아합니다.")
+print( name ,"는 어른일까요 ? " ,is_adult )
+
+# 애완동물을 소개해 주세요
+animal = "강아지"
+name = "호식이"
+age = 4
+hobby = "잠"
+is_adult = age >= 3 
+
+'''안녕하세요
+주석입니다
+지금배웟어요 '''
+
+
+
+
+
+print(" 우리집"  + animal + "있으며 이름은 "+ name + "입니다")
+print (  name + "는" + str(age) + "살이며 " + hobby + " 을 자는것을 좋아합니다.")
+print( name +"는 어른일까요 ? " + str(is_adult))
+
+# 열차출발 
+
+station1 = "사당"
+station2 = "신도림"
+station3 = "인천공항"
+
+print(station1, "행 열차가 들어오고 있습니다")
+print(station2+ "행 열차가 들어오고 있습니다")
+print(station3, "행 열차가 들어오고 있습니다.")
+
+print(1+1)
+print(3-2)
+print(5*2)
+print(5**4) #5의 네제곱
+print(10%4) # 10을 4로 나눈 나머지
+print(200//2) # 200을 2로 나눈 몫
+print(10 >3 ) # true
+print(4>=7) #false
+print(3 == 3 ) #똑같다는 의미 ture
+print(4==2 ) #false
+print(3+2-1==4 )#true
+
+print(1 !=3 ) #앞뒤가 같지않다 true
+print(not(1 != 2)) #false
+
+print((3 >0 )and (3 <5 )) #true 
+print((3 > 0 ) & (3 <5)) #true
+
+print((3 >0 )or (3 >5 )) #true
+print((3<5)| (3>2)) #true
+
+print(2 +3 *4) #14
+number = 2 + 3 *4  #14
+print(number)
+number = number + 2 # 16
+print(number)
+number *=3 
+print(number)
+number -=10
+print(number) # 38 
+number %=3 
+print(number) #2
+
+#숫자처리함수
+
+print(abs(-5)) # 절대값함수
+print(pow (4,2)) # 4^2 제곱함수
+print(max (5,12 )) # 뭐가 더 큰지
+print(min(5,12 )) # 뭐가 더 작은지
+print(round(15.57)) #반올림
+
+from math import *
+print(floor(15.57)) #내림
+print(ceil(15.57)) #올림 
+print(sqrt(16)) #제곱근 
+
+from random import *
+
+print (random()) # 0.0 이상 1. 미만의 임의의 값 생성
+print(random()*10) #10미만 임의의값
+
+print(int(random()*5)) #0부터 10미만의 정수  
+print(int(random()*5)) #0부터 10미만의 정수 
+print(int(random()*5)) #0부터 10미만의 정수 
+print(int(random()*5)) #0부터 10미만의 정수 
+print(int(random()*5)) #0부터 10미만의 정수
+
+print(int(random()*10+1)) #1부터 10 이하의 임의의값생성
+
+print(int(random()*45+1))
+
+print(int(random()*45+1))
+print(int(random()*45+1))
+print(int(random()*45+1))
+print(int(random()*45+1))
+print(int(random()*45+1))
